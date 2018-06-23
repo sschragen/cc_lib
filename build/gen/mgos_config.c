a9a9899cc7da63ddca797a7775ae6b0f71cd736b
Stephanus Schragen <stephanus@schragen-net.de>
2018-06-23 07:36:58 +0200

    test

---
+++ build/gen/mgos_config.c
@@ -0,0 +1,170 @@
+/* Generated file - do not edit. */
+
+#include <stddef.h>
+#include "mgos_config.h"
+
+const struct mgos_conf_entry mgos_config_schema_[24] = {
+  {.type = CONF_TYPE_OBJECT, .key = "", .num_desc = 23},
+  {.type = CONF_TYPE_OBJECT, .key = "device", .num_desc = 2},
+  {.type = CONF_TYPE_STRING, .key = "id", .offset = offsetof(struct mgos_config, device.id)},
+  {.type = CONF_TYPE_STRING, .key = "password", .offset = offsetof(struct mgos_config, device.password)},
+  {.type = CONF_TYPE_OBJECT, .key = "debug", .num_desc = 8},
+  {.type = CONF_TYPE_STRING, .key = "udp_log_addr", .offset = offsetof(struct mgos_config, debug.udp_log_addr)},
+  {.type = CONF_TYPE_INT, .key = "mbedtls_level", .offset = offsetof(struct mgos_config, debug.mbedtls_level)},
+  {.type = CONF_TYPE_INT, .key = "level", .offset = offsetof(struct mgos_config, debug.level)},
+  {.type = CONF_TYPE_STRING, .key = "filter", .offset = offsetof(struct mgos_config, debug.filter)},
+  {.type = CONF_TYPE_INT, .key = "stdout_uart", .offset = offsetof(struct mgos_config, debug.stdout_uart)},
+  {.type = CONF_TYPE_INT, .key = "stderr_uart", .offset = offsetof(struct mgos_config, debug.stderr_uart)},
+  {.type = CONF_TYPE_INT, .key = "factory_reset_gpio", .offset = offsetof(struct mgos_config, debug.factory_reset_gpio)},
+  {.type = CONF_TYPE_STRING, .key = "mg_mgr_hexdump_file", .offset = offsetof(struct mgos_config, debug.mg_mgr_hexdump_file)},
+  {.type = CONF_TYPE_OBJECT, .key = "sys", .num_desc = 9},
+  {.type = CONF_TYPE_OBJECT, .key = "mount", .num_desc = 5},
+  {.type = CONF_TYPE_STRING, .key = "path", .offset = offsetof(struct mgos_config, sys.mount.path)},
+  {.type = CONF_TYPE_STRING, .key = "dev_type", .offset = offsetof(struct mgos_config, sys.mount.dev_type)},
+  {.type = CONF_TYPE_STRING, .key = "dev_opts", .offset = offsetof(struct mgos_config, sys.mount.dev_opts)},
+  {.type = CONF_TYPE_STRING, .key = "fs_type", .offset = offsetof(struct mgos_config, sys.mount.fs_type)},
+  {.type = CONF_TYPE_STRING, .key = "fs_opts", .offset = offsetof(struct mgos_config, sys.mount.fs_opts)},
+  {.type = CONF_TYPE_STRING, .key = "tz_spec", .offset = offsetof(struct mgos_config, sys.tz_spec)},
+  {.type = CONF_TYPE_INT, .key = "wdt_timeout", .offset = offsetof(struct mgos_config, sys.wdt_timeout)},
+  {.type = CONF_TYPE_STRING, .key = "pref_ota_lib", .offset = offsetof(struct mgos_config, sys.pref_ota_lib)},
+  {.type = CONF_TYPE_STRING, .key = "conf_acl", .offset = offsetof(struct mgos_config, conf_acl)},
+};
+
+const struct mgos_conf_entry *mgos_config_schema() {
+  return mgos_config_schema_;
+}
+
+/* Global instance */
+struct mgos_config mgos_sys_config;
+
+/* Getters {{{ */
+const struct mgos_config_device *mgos_config_get_device(struct mgos_config *cfg) {
+  return &cfg->device;
+}
+const char *mgos_config_get_device_id(struct mgos_config *cfg) {
+  return cfg->device.id;
+}
+const char *mgos_config_get_device_password(struct mgos_config *cfg) {
+  return cfg->device.password;
+}
+const struct mgos_config_debug *mgos_config_get_debug(struct mgos_config *cfg) {
+  return &cfg->debug;
+}
+const char *mgos_config_get_debug_udp_log_addr(struct mgos_config *cfg) {
+  return cfg->debug.udp_log_addr;
+}
+int         mgos_config_get_debug_mbedtls_level(struct mgos_config *cfg) {
+  return cfg->debug.mbedtls_level;
+}
+int         mgos_config_get_debug_level(struct mgos_config *cfg) {
+  return cfg->debug.level;
+}
+const char *mgos_config_get_debug_filter(struct mgos_config *cfg) {
+  return cfg->debug.filter;
+}
+int         mgos_config_get_debug_stdout_uart(struct mgos_config *cfg) {
+  return cfg->debug.stdout_uart;
+}
+int         mgos_config_get_debug_stderr_uart(struct mgos_config *cfg) {
+  return cfg->debug.stderr_uart;
+}
+int         mgos_config_get_debug_factory_reset_gpio(struct mgos_config *cfg) {
+  return cfg->debug.factory_reset_gpio;
+}
+const char *mgos_config_get_debug_mg_mgr_hexdump_file(struct mgos_config *cfg) {
+  return cfg->debug.mg_mgr_hexdump_file;
+}
+const struct mgos_config_sys *mgos_config_get_sys(struct mgos_config *cfg) {
+  return &cfg->sys;
+}
+const struct mgos_config_sys_mount *mgos_config_get_sys_mount(struct mgos_config *cfg) {
+  return &cfg->sys.mount;
+}
+const char *mgos_config_get_sys_mount_path(struct mgos_config *cfg) {
+  return cfg->sys.mount.path;
+}
+const char *mgos_config_get_sys_mount_dev_type(struct mgos_config *cfg) {
+  return cfg->sys.mount.dev_type;
+}
+const char *mgos_config_get_sys_mount_dev_opts(struct mgos_config *cfg) {
+  return cfg->sys.mount.dev_opts;
+}
+const char *mgos_config_get_sys_mount_fs_type(struct mgos_config *cfg) {
+  return cfg->sys.mount.fs_type;
+}
+const char *mgos_config_get_sys_mount_fs_opts(struct mgos_config *cfg) {
+  return cfg->sys.mount.fs_opts;
+}
+const char *mgos_config_get_sys_tz_spec(struct mgos_config *cfg) {
+  return cfg->sys.tz_spec;
+}
+int         mgos_config_get_sys_wdt_timeout(struct mgos_config *cfg) {
+  return cfg->sys.wdt_timeout;
+}
+const char *mgos_config_get_sys_pref_ota_lib(struct mgos_config *cfg) {
+  return cfg->sys.pref_ota_lib;
+}
+const char *mgos_config_get_conf_acl(struct mgos_config *cfg) {
+  return cfg->conf_acl;
+}
+/* }}} */
+
+/* Setters {{{ */
+void mgos_config_set_device_id(struct mgos_config *cfg, const char *val) {
+  mgos_conf_set_str(&cfg->device.id, val);
+}
+void mgos_config_set_device_password(struct mgos_config *cfg, const char *val) {
+  mgos_conf_set_str(&cfg->device.password, val);
+}
+void mgos_config_set_debug_udp_log_addr(struct mgos_config *cfg, const char *val) {
+  mgos_conf_set_str(&cfg->debug.udp_log_addr, val);
+}
+void mgos_config_set_debug_mbedtls_level(struct mgos_config *cfg, int         val) {
+  cfg->debug.mbedtls_level = val;
+}
+void mgos_config_set_debug_level(struct mgos_config *cfg, int         val) {
+  cfg->debug.level = val;
+}
+void mgos_config_set_debug_filter(struct mgos_config *cfg, const char *val) {
+  mgos_conf_set_str(&cfg->debug.filter, val);
+}
+void mgos_config_set_debug_stdout_uart(struct mgos_config *cfg, int         val) {
+  cfg->debug.stdout_uart = val;
+}
+void mgos_config_set_debug_stderr_uart(struct mgos_config *cfg, int         val) {
+  cfg->debug.stderr_uart = val;
+}
+void mgos_config_set_debug_factory_reset_gpio(struct mgos_config *cfg, int         val) {
+  cfg->debug.factory_reset_gpio = val;
+}
+void mgos_config_set_debug_mg_mgr_hexdump_file(struct mgos_config *cfg, const char *val) {
+  mgos_conf_set_str(&cfg->debug.mg_mgr_hexdump_file, val);
+}
+void mgos_config_set_sys_mount_path(struct mgos_config *cfg, const char *val) {
+  mgos_conf_set_str(&cfg->sys.mount.path, val);
+}
+void mgos_config_set_sys_mount_dev_type(struct mgos_config *cfg, const char *val) {
+  mgos_conf_set_str(&cfg->sys.mount.dev_type, val);
+}
+void mgos_config_set_sys_mount_dev_opts(struct mgos_config *cfg, const char *val) {
+  mgos_conf_set_str(&cfg->sys.mount.dev_opts, val);
+}
+void mgos_config_set_sys_mount_fs_type(struct mgos_config *cfg, const char *val) {
+  mgos_conf_set_str(&cfg->sys.mount.fs_type, val);
+}
+void mgos_config_set_sys_mount_fs_opts(struct mgos_config *cfg, const char *val) {
+  mgos_conf_set_str(&cfg->sys.mount.fs_opts, val);
+}
+void mgos_config_set_sys_tz_spec(struct mgos_config *cfg, const char *val) {
+  mgos_conf_set_str(&cfg->sys.tz_spec, val);
+}
+void mgos_config_set_sys_wdt_timeout(struct mgos_config *cfg, int         val) {
+  cfg->sys.wdt_timeout = val;
+}
+void mgos_config_set_sys_pref_ota_lib(struct mgos_config *cfg, const char *val) {
+  mgos_conf_set_str(&cfg->sys.pref_ota_lib, val);
+}
+void mgos_config_set_conf_acl(struct mgos_config *cfg, const char *val) {
+  mgos_conf_set_str(&cfg->conf_acl, val);
+}
+/* }}} */
---
+++ build/gen/vars.mk
@@ -0,0 +1,16 @@
+APP=cc_lib
+APP_BIN_LIBS=
+APP_CFLAGS=
+APP_CXXFLAGS=
+APP_FS_FILES=
+APP_INCLUDES=/fwbuild-volumes/2.3/apps/cc_lib/esp8266/build_contexts/build_ctx_186311031/include
+APP_SOURCES=/fwbuild-volumes/2.3/apps/cc_lib/esp8266/build_contexts/build_ctx_186311031/src/empty_lib.c
+APP_VERSION=1.0
+BUILD_DIR=/fwbuild-volumes/2.3/apps/cc_lib/esp8266/build_contexts/build_ctx_186311031/build/objs
+FFI_SYMBOLS=
+FS_STAGING_DIR=/fwbuild-volumes/2.3/apps/cc_lib/esp8266/build_contexts/build_ctx_186311031/build/fs
+FW_DIR=/fwbuild-volumes/2.3/apps/cc_lib/esp8266/build_contexts/build_ctx_186311031/build/fw
+GEN_DIR=/fwbuild-volumes/2.3/apps/cc_lib/esp8266/build_contexts/build_ctx_186311031/build/gen
+MANIFEST_FINAL=/fwbuild-volumes/2.3/apps/cc_lib/esp8266/build_contexts/build_ctx_186311031/build/gen/mos_final.yml
+MGOS_PATH=/mongoose-os
+PLATFORM=esp8266
